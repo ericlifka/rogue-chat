6544332965466b0d49351575c2b2074822e00ed3
Eric Lifka <eric.lifka@genesys.com>
2018-06-15 21:33:32 -0400

    move auth logic into a session

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,26 +1,10 @@
-import Ember from 'ember';
-const {
-    Route
-} = Ember;
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
-    builtinClientId: '@@CLIENT_ID',
+    session: service(),
 
     beforeModel() {
-        const CLIENT_ID = this.get('builtinClientId');
-        console.log(`CLIENT_ID ${CLIENT_ID}`);
-
-        const platformClient = window.require('platformClient');
-        let client = platformClient.ApiClient.instance;
-        client.setEnvironment('inindca.com');
-        return client.loginImplicitGrant(CLIENT_ID, `${window.location.origin}${window.location.pathname}`)
-            .then(function(data) {
-                console.log(data);
-                // Do authenticated things
-            })
-            .catch(function(err) {
-                // Handle failure response
-                console.log(err);
-            });
+        return this.get('session').authenticate();
     }
 });
---
+++ app/services/session.js
@@ -0,0 +1,19 @@
+import {computed} from '@ember/object';
+import Service from '@ember/service';
+
+export default Service.extend({
+
+    clientId: '@@CLIENT_ID',
+    redirectUri: computed(() => `${window.location.origin}${window.location.pathname}`),
+
+    authenticate() {
+        const platformClient = window.require('platformClient');
+        platformClient.ApiClient.instance.setEnvironment('inindca.com');
+
+        return platformClient.ApiClient.instance
+            .loginImplicitGrant(this.get('clientId'), this.get('redirectUri'))
+            .then(({accessToken}) =>
+                this.set('accessToken', accessToken));
+    }
+
+});
