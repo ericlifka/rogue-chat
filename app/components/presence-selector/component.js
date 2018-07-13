068fff2a5384d02cb547fc98df3a06acd3dcff1b
Arin Reese <arin.reese@inin.com>
2018-07-13 10:30:33 -0400

    [ROGUE]Inject presence service into presence selector.

--- app/components/presence-selector/component.js
+++ app/components/presence-selector/component.js
@@ -1,5 +1,12 @@
+import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 
 export default Component.extend({
+    presence: service(),
 
+    init() {
+        this._super(...arguments);
+        const presences = this.get('presence.presences');
+        console.log('Presences: ', presences);
+    }
 });
