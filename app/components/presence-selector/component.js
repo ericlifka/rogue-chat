9c29ce2e3ac41352365c11bb1ef155a128b5e253
Arin Reese <arin.reese@inin.com>
2018-07-15 15:37:03 -0400

    [ROGUE]Load profile picture on presence bar.

--- app/components/presence-selector/component.js
+++ app/components/presence-selector/component.js
@@ -2,8 +2,10 @@ import { inject as service } from '@ember/service';
 import { reads } from '@ember/object/computed';
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import _ from 'lodash';
 
 //TODO: localize system presences or fetch from presence service for localization
+//TODO: create application service to fetch domain from for requests
 
 export default Component.extend({
     presence: service(),
@@ -14,6 +16,11 @@ export default Component.extend({
     presences: reads('presence.presences'),
     user: reads('session.user'),
     presenceLabel: reads('user.presence.systemPresence'),
+
+    profileImageUrl: computed('user.images.[]', function () {
+        const imageUrl = _.find(this.get('user.images'), {resolution: 'x96'});
+        return _.get(imageUrl, 'imageUri', 'https://apps.inindca.com/static-resources/avatar-x96.png');
+    }),
 
     presenceClass: computed('user.presence.systemPresence', function () {
         return this.get('session.user.presence.systemPresence').toLowerCase().replace(' ', '-');
