aacb82a50cca33be5abb65276651be94115b394f
Arin Reese <arin.reese@inin.com>
2018-07-15 16:24:25 -0400

    [ROGUE]Close picker after selecting presence.

--- app/components/presence-selector/component.js
+++ app/components/presence-selector/component.js
@@ -41,6 +41,7 @@ export default Component.extend({
 
         setPresence(presence) {
             this.get('presence').setUserPresence(presence);
+            this.toggleProperty('showPresencePicker');
         }
     }
 });
