0170e4355864b4641e7f28c93be602ed3a197ac0
Arin Reese <arin.reese@inin.com>
2018-08-05 15:56:35 -0400

    [ROGUE]Pull markdown into util.

--- app/components/interaction-messages/message/component.js
+++ app/components/interaction-messages/message/component.js
@@ -1,8 +1,7 @@
+import { markdownToHTML } from '../../../utils/markdown'
 import { reads } from '@ember/object/computed';
 import { computed } from '@ember/object';
 import Component from '@ember/component';
-import Markdown from 'markdown-it';
-import hljs from 'highlight.js';
 import _ from 'lodash';
 
 export default Component.extend({
@@ -10,37 +9,10 @@ export default Component.extend({
     markdownParser: null,
     message: null,
 
-    init() {
-        this._super(...arguments);
-        hljs.configure({
-            languages: ['bash', 'clojure', 'coffeescript', 'cpp', 'cs', 'css', 'elm', 'go', 'xml',
-                'handlebars', 'http', 'ini', 'java', 'javascript', 'json', 'makefile', 'objectivec',
-                'python', 'ruby', 'rust', 'sql', 'swift', 'typescript']
-        });
-        this.markdownParser = new Markdown({
-            html: false,
-            linkify: true,
-            typographer: false,
-            highlight(code, lang) {
-                if (lang === 'no-highlight') {
-                    return Ember.Handlebars.Utils.escapeExpression(code);
-                } else if (lang) {
-                    try {
-                        return hljs.highlight(lang, code).value;
-                    } catch (e) {
-                        return Ember.Handlebars.Utils.escapeExpression(code);
-                    }
-                } else {
-                    return hljs.highlightAuto(code).value;
-                }
-            }
-        });
-    },
-
     user: reads('message.user'),
 
     markdown: computed('message.raw', function () {
-        return this.markdownParser.render(this.get('message.raw'));
+        return markdownToHTML(this.get('message.raw'));
     }),
 
     profileImageUrl: computed('user.images.[]', function () {
---
+++ app/utils/markdown.js
@@ -0,0 +1,35 @@
+import Markdown from "markdown-it";
+import hljs from "highlight.js";
+
+hljs.configure({
+    languages: ['bash', 'clojure', 'coffeescript', 'cpp', 'cs', 'css', 'elm', 'go', 'xml',
+        'handlebars', 'http', 'ini', 'java', 'javascript', 'json', 'makefile', 'objectivec',
+        'python', 'ruby', 'rust', 'sql', 'swift', 'typescript']
+});
+
+const markdownParser = new Markdown({
+    html: false,
+    linkify: true,
+    typographer: false,
+    highlight(code, lang) {
+        if (lang === 'no-highlight') {
+            return Ember.Handlebars.Utils.escapeExpression(code);
+        } else if (lang) {
+            try {
+                return hljs.highlight(lang, code).value;
+            } catch (e) {
+                return Ember.Handlebars.Utils.escapeExpression(code);
+            }
+        } else {
+            return hljs.highlightAuto(code).value;
+        }
+    }
+});
+
+function markdownToHTML (markdown) {
+    return markdownParser.render(markdown);
+}
+
+export {
+    markdownToHTML
+}
