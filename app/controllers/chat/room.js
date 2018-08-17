802d6009ef8e2b1cf5e6f8aa397dbe081d2bfafd
Arin Reese <arin.reese@inin.com>
2018-08-16 20:51:04 -0400

    [ROGUE]Get everything set up for panels.

--- app/controllers/chat/room.js
+++ app/controllers/chat/room.js
@@ -1,4 +1,5 @@
 import { inject as service } from '@ember/service';
+import { equal } from '@ember/object/computed';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
@@ -6,10 +7,22 @@ export default Controller.extend({
 
     chat: service(),
 
+    activePanel: null,
+
+    showSearchPanel: equal('activePanel', 'search'),
+    showInvitePanel: equal('activePanel', 'invite'),
+
     actions: {
         sendMessage(message) {
             const model = this.get('model');
             this.get('chat').sendMessage(model, message);
+        },
+
+        togglePanel(panel) {
+            if (this.get('activePanel') === panel) {
+                return this.set('activePanel', null);
+            }
+            this.set('activePanel', panel);
         }
     }
 });
