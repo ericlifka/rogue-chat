48353797a0cb917e919a3e530f9f3e3e27cdee05
Arin Reese <arin.reese@inin.com>
2018-08-23 23:57:16 -0400

    [ROGUE]Add basic history search support.

---
+++ app/components/history-panel/history-message/component.js
@@ -0,0 +1,20 @@
+import { reads } from '@ember/object/computed';
+import { computed } from '@ember/object';
+import Component from '@ember/component';
+
+export default Component.extend({
+    classNames: ['history-message'],
+
+    message: null,
+
+    user: reads('message.user'),
+    markdown: reads('message.markdown'),
+
+    name: computed('user.name', function () {
+        return this.get('user.name').toLowerCase();
+    }),
+
+    time: computed('message.time', function () {
+        return this.get('message.time').format('D MMM hh:mma');
+    }),
+});
