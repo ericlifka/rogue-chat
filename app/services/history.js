2057fdb5d467fcb0a36ac748d2149b664cab6750
Arin Reese <arin.reese@inin.com>
2018-08-26 20:14:14 -0400

    [ROGUE]Update search service to fetch history without room.

--- app/services/history.js
+++ app/services/history.js
@@ -2,6 +2,7 @@ import { inject as service } from '@ember/service';
 import Service from '@ember/service';
 import Ember from 'ember';
 import RSVP from 'rsvp';
+import uuid from 'uuid';
 
 export default Service.extend({
     ipc: service(),
@@ -9,37 +10,53 @@ export default Service.extend({
 
     messageHistoryRequestDefault: 25,
 
-    requestHistory(room, options) {
+    requestHistory(id, options) {
+        // Realtime only supports fetching up to one hundred messages at a time
+        options.limit = Math.min(options.limit, 100) || this.get('messageHistoryRequestDefault');
+
+        return new RSVP.Promise((resolve, reject) => {
+            // Create a timeout just in case we don't get a response from realtime
+            const tid = setTimeout(() => {
+                reject(new Error('Never received a history response from realtime'));
+            }, 10000);
+
+            const scopedHistoryTopic = `history:${id}`;
+            this.get('ipc').registerOneTimeListener(scopedHistoryTopic, (event, messages) => {
+                clearTimeout(tid);
+                // realtime emits null if there is no history so we can't use default assignment
+                resolve(messages || []);
+            });
+
+            this.get('ipc').sendEvent('request-history', {
+                id,
+                payload: options
+            });
+        });
+    },
+
+    loadHistoryWithoutRoom(options) {
+        const id = uuid.v4();
+        return this.requestHistory(id, options)
+            .then(async messages => {
+                const messagePromises = messages.map(message => {
+                    return this.get('chat').setupMessageModel(message);
+                });
+                return await RSVP.all(messagePromises);
+            })
+            .catch(error => {
+                Ember.Logger.error('Failed to fetch history from realtime, error: ', error);
+                return error;
+            })
+    },
+
+    loadRoomHistory(room, options) {
         // You must have joined the room before requesting history
         if (!room.get('activated') && !(room.get('type') === 'person')) {
             throw new Error('room must be activated before requesting history');
         }
-        // Realtime only supports fetching up to one hundred messages at a time
-        options.limit = Math.min(options.limit, 100) || this.get('messageHistoryRequestDefault');
-
-        const defer = RSVP.defer();
-        // Create a timeout just in case we don't get a response from realtime
-        const tid = setTimeout(() => {
-            defer.reject(new Error('Never received a history response from realtime'));
-        }, 5000);
-
-        const scopedHistoryTopic = `history:${room.get('id')}`;
-        this.get('ipc').registerOneTimeListener(scopedHistoryTopic, (event, messages) => {
-            clearTimeout(tid);
-            defer.resolve(messages);
-        });
-
         room.set('loadingHistory', true);
-        this.get('ipc').sendEvent('request-history', {
-            id: room.get('id'),
-            payload: options
-        });
 
-        return defer.promise;
-    },
-
-    loadRoomHistory(room, options) {
-        return this.requestHistory(room, options)
+        return this.requestHistory(room.get('id'), options)
             .then(async messages => {
                 const messagePromises = messages.map(message => {
                     return this.get('chat').setupMessageModel(message);
